229be99775e1cc5f5a41289504ea758471e253c0
sivachelvan2002 <s.sivachelvan2002@gmail.com>
2025-11-17 06:02:47 +0530

    Improve UI styles and update color scheme
    Enhanced the table header, sidebar, and footer background colors for a more cohesive look. Added and refined CSS for centering, font size, and navigation list styling, including hover effects and color adjustments for better user experience.

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ require_once('config.php');?>
     <title>School-System</title>
     <style>
         td[colspan="2"]{
-
+            text-align: center;
+            font-size: xx-large;
+            color: whitesmoke;
         }
         img{
             width: "100px";
@@ -16,16 +18,46 @@ require_once('config.php');?>
             border-radius: 50%;
             justify-content: ;
         }
+        ul{
+            list-style: none;
+            display: flex;
+            flex-direction: column;
+            text-decoration: none;
+            
+            
+        }
+        ul li{
+            margin: 0;
+            padding: 10px 10px;
+            justify-content: center;
+            align-items: center;
+            
+            
+           
+        }
+        ul li:hover{
+            background-color: pink;
+            border-radius: 10px;
+            color: black;
+            
+
+        }
+        ul li a{
+            color: whitesmoke;
+        }
+        ul li a:hover{
+            color: black;
+        }
    
     </style>
 </head>
 <body>
     <table width="100%" height="100%">
-        <tr height="100" style="background-color: #ccc;">
+        <tr height="100" style="background-color: #3b2e58ff;">
             <td colspan="2" >School Management system</td>
         </tr>
         <tr height="400">
-            <td width="15%" style="background-color: #62749eff;" >
+            <td width="15%" style="background-color: #3b2e58ff;" >
                 <ul>
                     <li><a href="?section=student&page=index">Student</a></li>
                     <li><a href="?section=subject&page=index">Subject</a></li>
@@ -59,7 +91,7 @@ require_once('config.php');?>
                 ?>
             </td>
         </tr>
-        <tr height="100" style="background-color: #ccc;">
+        <tr height="100" style="background-color: #3b2e58ff;">
             <td colspan="2" >Footer</td>
         </tr>
     </table>
